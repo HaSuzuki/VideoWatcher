a384ecb1703703d87544d0974964c95f67146d89
hakutosuzuki <14cm2425252@gmail.com>
2018-03-03 23:29:12 +0900

    add model

---
+++ app/src/main/java/jp/co/hakutosuzuki/videowatcher/ui/model/VideoItemModel.kt
@@ -0,0 +1,10 @@
+package jp.co.hakutosuzuki.videowatcher.ui.model
+
+/**
+ * Created by hakutosuzuki on 2018/03/03.
+ */
+data class VideoItemModel (
+        val name: String,
+        val filePath: String,
+        val playTime: Int
+)
