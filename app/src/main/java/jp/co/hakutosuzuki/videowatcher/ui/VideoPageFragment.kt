bfd270031cb68d3578e8d507d675f539699d8dd9
hakutosuzuki <14cm2425252@gmail.com>
2018-03-04 00:04:00 +0900

    add recycler

--- app/src/main/java/jp/co/hakutosuzuki/videowatcher/ui/VideoPageFragment.kt
+++ app/src/main/java/jp/co/hakutosuzuki/videowatcher/ui/VideoPageFragment.kt
@@ -1,15 +1,21 @@
 package jp.co.hakutosuzuki.videowatcher.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jp.co.hakutosuzuki.videowatcher.databinding.FragmentVideoPageBinding
 
 class VideoPageFragment : Fragment() {
+    companion object {
+        val gridSpanSize: Int = 2
+    }
 
     private lateinit var binding: FragmentVideoPageBinding
+    private lateinit var mContext: Context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -18,7 +24,13 @@ class VideoPageFragment : Fragment() {
                 container!!,
                 false
         )
+        mContext = activity!!.applicationContext
         activity?.supportStartPostponedEnterTransition()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.layoutManager = GridLayoutManager(mContext, gridSpanSize)
     }
 }
